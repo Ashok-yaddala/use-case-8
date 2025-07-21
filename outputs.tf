50b7798e34d060f162cbd5bd76cb7b15cb8a1d7d
Ashok-yaddala <Ashokyaddala4devops@gmail.com>
2025-07-21 19:56:57 +0530

    changes in output

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,34 @@
 output "vpc_id" {
   description = "ID of the VPC"
-  value       = modules.vpc.vpc_id
+  value       = module.vpc.vpc_id
 }
 
 output "alb_dns_name" {
   description = "DNS name of the load balancer"
-  value       = modules.alb.alb_dns_name
+  value       = module.alb.alb_dns_name
 }
 
 output "alb_zone_id" {
   description = "Zone ID of the load balancer"
-  value       = modules.alb.alb_zone_id
+  value       = module.alb.alb_zone_id
 }
 
 output "ecr_patient_repository_url" {
   description = "URL of the patient service ECR repository"
-  value       = modules.ecr.patient_service_repository_url
+  value       = module.ecr.patient_service_repository_url
 }
 
 output "ecr_appointment_repository_url" {
   description = "URL of the appointment service ECR repository"
-  value       = modules.ecr.appointment_service_repository_url
+  value       = module.ecr.appointment_service_repository_url
 }
 
 output "ecs_cluster_name" {
   description = "Name of the ECS cluster"
-  value       = modules.ecs.cluster_name
+  value       = module.ecs.cluster_name
 }
 
 output "cloudwatch_log_group_names" {
   description = "Names of CloudWatch log groups"
-  value       = modules.cloudwatch.log_group_names
+  value       = module.cloudwatch.log_group_names
 }
